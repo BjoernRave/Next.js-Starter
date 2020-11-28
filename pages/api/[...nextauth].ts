18183661f3cfd7a301eed2b17e031e74ae90e0b3
BjoernRave <bjoern.rave@gmail.com>
2020-11-28 01:46:18 +0100

    add next-auth

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+import NextAuth from 'next-auth'
+import Adapters from 'next-auth/adapters'
+import Providers from 'next-auth/providers'
+
+declare namespace globalThis {
+  let prisma: PrismaClient
+}
+
+function getPersistentPrismaClient() {
+  // Ensure the Prisma instance is re-used during hot-reloading
+  // Otherwise, a new client would be created on every reload
+  globalThis.prisma ||= new PrismaClient()
+  return globalThis.prisma
+}
+
+const prisma =
+  process.env.NODE_ENV === 'production'
+    ? new PrismaClient()
+    : getPersistentPrismaClient()
+
+const options = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+}
+
+export default (req, res) => NextAuth(req, res, options)
